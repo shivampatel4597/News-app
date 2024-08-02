fec97d5d87d888ca2de4389bb5430168af2f6514
shivam patel <shivamsinghpatel4597@gmail.com>
2024-08-03 02:30:56 +0530

    added vercel.json

--- src/components/NewsBoard.jsx
+++ src/components/NewsBoard.jsx
@@ -7,7 +7,7 @@ const NewsBoard = ({category}) => {
 const[articles, setArticles] = useState([]);
 
 useEffect(()=>{
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`
     fetch(url).then(response => response.json()).then(data=>setArticles(data.articles))
 },[category])
 
@@ -15,6 +15,7 @@ useEffect(()=>{
     <div>
       <h2 className="text-center"> Latest <span className="badge bg-danger">News</span></h2>
       <div className="text-center">
+        {console.log(articles)}
       {articles.map((news,index)=>{
         return <NewsItem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url}/>
       })}
